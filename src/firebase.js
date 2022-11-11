5b5b2f6062e04f1437f06b92235ded393129a49c
Brandon Deen <MisterDeen1694@gmail.com>
2022-11-11 02:11:03 +0100

    Init Commit + nav bar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyBfZkcD9YrcY7qVWhfEnDP7pFNl0CACdoA',
+    authDomain: 'clone-46a86.firebaseapp.com',
+    projectId: 'clone-46a86',
+    storageBucket: 'clone-46a86.appspot.com',
+    messagingSenderId: '132614010504',
+    appId: '1:132614010504:web:f2d32c2a1068bc162ed13d',
+    measurementId: 'G-BZDDYBC3SQ',
+};
